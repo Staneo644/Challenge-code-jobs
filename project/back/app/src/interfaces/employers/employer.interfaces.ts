5af24f8aa31793d1cac1dc2c6ef3f6d3c48763e4
Staneo644 <aurele.josserand@gmail.com>
2023-09-29 22:56:08 +0200

    delete interfaces

--- project/back/app/src/interfaces/employers/employer.interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { Employer } from '../../applications/employers/employer.entity';
-
-export interface IEmployersDomain {
-  // createEmployer(employerData: Employer): Promise<Employer | null>;
-  // updateEmployer(email: string, employerData: Partial<Employer>): Promise<Employer | null>;
-  // isEmployer(email: string): Promise<boolean>;
-  // deleteEmployer(email: string): Promise<void>;
-  // getEmployer(email: string): Promise<Employer | null>;
-}
